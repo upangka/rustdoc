b84dc98cbc5c312af3bb7933f0cf2fdd56d303c5
pkmer <q10viking@outlook.com>
2025-07-08 17:35:06 +0800

    feat: :sparkles: add guessing game

---
+++ b-guessing-game/src/main.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::io::stdin;
+use std::cmp::Ordering;
+fn main() {
+    let mut guess = String::new();
+    // 使用rand生成随机的数字范围1..=100
+
+    let answer: u32 = rand::rng().random_range(1..=100);
+    println!("答案为: {answer}");
+
+    loop {
+        // 清空
+        guess.clear();
+        println!("请输入猜测的数字");
+        // 会追加到指定内存区域
+        stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadowing
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("输入错误: {}", e);
+                continue
+            },
+        };
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("猜小了"),
+            Ordering::Greater => println!("猜大了"),
+            Ordering::Equal => {
+                println!("恭喜你猜对了");
+                break;
+            }
+        }
+    }
+}
+
