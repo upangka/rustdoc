bbd35a1f48932c95b25d9a376576249cf5cbbf84
pkmer <q10viking@outlook.com>
2025-07-10 23:11:06 +0800

    feat: :sparkles: finished

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,5 @@
-
+use minigrep::{self, Config};
 use std::{env, process};
-use minigrep::{self,Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,12 +10,11 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
-
