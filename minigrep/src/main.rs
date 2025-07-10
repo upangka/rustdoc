0e38d05f212ec11ebb2fe264f185297a97ff6e5e
pkmer <q10viking@outlook.com>
2025-07-10 21:44:14 +0800

    feat: :sparkles: 重构这一块

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,35 @@
-use std::env;
+use std::fs;
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+
+    (query, file_path)
 }
