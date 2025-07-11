2584eb54682d7c2ea9a8898a69c6ee75e5a7311f
pkmer <q10viking@outlook.com>
2025-07-11 08:54:00 +0800

    feat: :sparkles: 闭包closure

---
+++ n-closure/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // 没有声明类型
+    let example_closure = |x| x;
+    // 提前锁定闭包的类型 fn(String) -> String
+    let s = example_closure(String::from("hello"));
+    // 编译错误
+    // let n = example_closure(5);
+
+    let a = 3;
+    println!("{} {}", a, &a);
+
+    let list = vec![1, 2, 3];
+    println!("1: {list:?}");
+    println!("2: {:?}",&list);
+}
