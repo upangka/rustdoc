8745b0400e063cd2ee38280a50cffcdefe7e9547
pkmer <q10viking@outlook.com>
2025-07-15 09:52:57 +0800

    finished

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -48,13 +48,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    //  迭代器优化
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+    
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
