270695a33197d57dcaf1c5c72a7636473296f97d
pkmer <q10viking@outlook.com>
2025-07-26 15:10:09 +0800

    feat: :sparkles: finished basic anyhow

--- anyhow-practice/src/main.rs
+++ anyhow-practice/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+
+use anyhow::{anyhow,Context,Result};
+
+fn main() -> Result<()>{
     println!("Hello, world!");
+     // might_fail(false).with_context(|| "might_fail() failed")?;
+     might_fail(false).context("might_fail() failed")?;
+    Ok(())
+}
+
+
+fn might_fail(succeed: bool) -> Result<()> {
+    if succeed{
+        Ok(())
+    }else{
+        Err(anyhow!("可能发生的错误"))
+    }
 }
