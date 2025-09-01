67de208156272da500b997d4702dd9355825f83b
pkmer <q10viking@outlook.com>
2025-09-01 22:29:30 +0800

    feat: :sparkles: finished quate shorthand code

--- error-practice/src/quote.rs
+++ error-practice/src/quote.rs
@@ -1,7 +1,7 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, Read};
 
-fn read_username_from_file() -> Result<String, io::Error> {
+fn read_username_from_file_v1() -> Result<String, io::Error> {
     // Result<File>
     let username_file_result = File::open("hello.txt");
 
@@ -18,4 +18,25 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username), // _占位符，不绑定值
         Err(error) => Err(error),
     }
+}
+
+fn read_username_from_file_v2() -> Result<String, io::Error> {
+    let mut username = String::new();
+    // File
+    let mut username_file = File::open("hello.txt")?;
+
+    // 这里不需要变量接收，只需要处理Error的情况
+    username_file.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file_v3() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_v4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
