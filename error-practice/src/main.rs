8a6b8fcb007698d794bde3b09f634db0f3b409b6
pkmer <q10viking@outlook.com>
2025-09-01 23:56:59 +0800

    feat: :sparkles: finished error handling

--- error-practice/src/main.rs
+++ error-practice/src/main.rs
@@ -2,11 +2,26 @@ mod quote;
 mod our_error;
 mod question_with_option;
 
-use std::fs::File;
 
 use std::io::{ErrorKind};
 
-fn main() {
+
+
+use std::fs::File;
+use std::error::Error;
+// fn main() -> Result<(),Box<dyn Error>>{
+//     println!("hello world");
+//     let _file = File::open("some_file.txt")?;
+//     Ok(())
+// }
+
+
+fn main(){
+    panic!("panic");
+    println!("hello world")
+}
+#[test]
+fn main_test() {
     let file = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
