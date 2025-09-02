63decbd2b248318b9ff0eb80ca934af5055f2e87
pkmer <q10viking@outlook.com>
2025-09-03 01:15:27 +0800

    feat: :sparkles: add lifetime code

---
+++ lifetime-pratice/src/main.rs
@@ -0,0 +1,36 @@
+// fn main() {
+//
+//     println!("{}",reverse("Hello, world!"));
+//     println!("{}",reverse_v2("Hello, world!"))
+// }
+
+
+pub fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+
+pub fn reverse(input: &str) -> String {
+    let mut s = String::new();
+    for c in input.chars().rev(){
+        s.push(c);
+    }
+    s
+}
+
+pub fn reverse_v2(input:&str) -> String{
+    input.chars().rev().collect()
+}
+
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}
